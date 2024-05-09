c7c81cc057ec45af048e0e2d7c67d72f12fa58e1
limpa95 <patrickhlim@gmail.com>
2024-05-09 14:19:50 -0500

    DMQ file edited

--- DMQ.sql
+++ DMQ.sql
@@ -1,6 +1,3 @@
--- Query for add a new character functionality with colon : character being used to 
--- denote the variables that will have data from the backend programming language
-
 -- get all genres for browse genres page
 SELECT genre_id, genre_name FROM `Genres`;
 
@@ -9,6 +6,9 @@ SELECT actor_id, actor_name FROM `Actors`;
 
 -- get all directors for browse directors page
 SELECT director_id, director_name FROM `Directors`;
+
+-- Query for add a new character functionality with colon : character being used to 
+-- denote the variables that will have data from the backend programming language
 
 -- add new genre
 INSERT INTO `Genres` (`genre_name`)
@@ -21,3 +21,15 @@ VALUES (:actor_nameInput);
 -- add new director
 INSERT INTO `Directors` (`director_name`)
 VALUES (:genre_nameInput);
+
+-- Query for delete a new character functionality with colon : character being used to 
+-- denote the variables that will have data from the backend programming language
+
+-- delete genre
+DELETE FROM `Genres` WHERE genre_id = :genre_ID_selected_from_browse_character_page
+
+-- delete actor
+DELETE FROM `Actors` WHERE actor_id = :actor_ID_selected_from_browse_character_page
+
+-- delete director
+DELETE FROM `Directors` WHERE director_id = :director_ID_selected_from_browse_character_page
