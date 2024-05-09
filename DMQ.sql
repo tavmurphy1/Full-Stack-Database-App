5239d44b6a5947eddbbfc05f3cec051d7ad543b5
limpa95 <patrickhlim@gmail.com>
2024-05-09 17:23:08 -0500

    updated DMQ and episodes page

--- DMQ.sql
+++ DMQ.sql
@@ -1,3 +1,6 @@
+-- get all genres for browse users page
+SELECT user_id, user_name, user_email, user_country FROM `Users`;
+
 -- get all genres for browse genres page
 SELECT genre_id, genre_name FROM `Genres`;
 
@@ -7,8 +10,18 @@ SELECT actor_id, actor_name FROM `Actors`;
 -- get all directors for browse directors page
 SELECT director_id, director_name FROM `Directors`;
 
+-- 
+SELECT episode_id, episode_title, episode_length, Televisions.television_title AS television_name
+	FROM Episodes
+		INNER JOIN Televisions
+        ON television_id_ep = Televisions.television_id
+
 -- Query for add a new character functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
+
+-- add new user
+INSERT INTO `Users` (`user_name`, `user_email`, `user_country`)
+VALUES (:user_nameInput, :user_emailInput, :user_countryInput);
 
 -- add new genre
 INSERT INTO `Genres` (`genre_name`)
