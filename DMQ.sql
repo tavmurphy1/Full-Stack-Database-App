c2e65f7d44124adc81ac2fe4f9c2a715adcc4ecf
limpa95 <patrickhlim@gmail.com>
2024-05-09 21:44:21 -0500

    updated DMQ for movies

--- DMQ.sql
+++ DMQ.sql
@@ -1,3 +1,5 @@
+-- Queries for read functionality
+
 -- get all genres for browse users page
 SELECT user_id, user_name, user_email, user_country FROM `Users`;
 
@@ -22,9 +24,42 @@ SELECT engagement_id, favorite, rating, view, user_id, Movies.movie_title AS mov
 		LEFT JOIN Movies
         ON Engagements.movie_id = Movies.movie_id 
 		LEFT JOIN Televisions
-        ON Engagements.television_id = Televisions.television_id 
+        ON Engagements.television_id = Televisions.television_id;
+        
+-- get all Movies for browse movies page
+SELECT Movies.movie_id, movie_title, movie_length, sum(Engagements.view)
+	FROM Movies
+		INNER JOIN Engagements
+        ON Engagements.movie_id = Movies.movie_id
+    group by Movies.movie_id
+    Order by Movies.movie_id;
+	
+-- get  associated genres, actors, directors for browsw movies page
+SELECT  Genres.genre_name AS `genres(s)`
+	FROM Movies_Genres
+		INNER JOIN Genres
+        ON Genres.genre_id = Movies_Genres.genre_id_mg
+        INNER JOIN Movies
+		ON Movies.movie_id = Movies_Genres.movie_id_mg
+	Order by Movies.movie_id;
+    
+SELECT Actors.actor_name AS `actor(s)`
+FROM Movies_Actors
+		INNER JOIN Actors
+        ON Actors.actor_id = Movies_Actors.actor_id_ma
+        INNER JOIN Movies
+		ON Movies.movie_id = Movies_Actors.movie_id_ma
+	Order by Movies.movie_id;
+    
+SELECT Directors.director_name AS `director(s)`
+FROM Movies_Directors
+		INNER JOIN Directors
+        ON Directors.director_id = Movies_Directors.director_id_md
+        INNER JOIN Movies
+		ON Movies.movie_id = Movies_Directors.movie_id_md
+	Order by Movies.movie_id
 
--- Query for add a new character functionality with colon : character being used to 
+-- Queries for insert functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
 
 -- add new user
@@ -43,16 +78,34 @@ VALUES (:actor_nameInput);
 INSERT INTO `Directors` (`director_name`)
 VALUES (:director_nameInput);
 
+-- get all television titles to populate the television title dropdown for new episode
+SELECT television_title FROM Televisions;
+
 -- add new episode
 INSERT INTO `Episodes` (`episode_title`, `episode_length`, `television_id_ep`)
 VALUES (:episode_titleInput, :episode_lengthInput, :television_id_from_dropdown_Input,);
+
+-- get all movie titles to populate the movie title dropdown for new engagement
+SELECT movie_title FROM Televisions;
+
+-- get all television titles to populate the television title dropdown for new engagement
+SELECT television_title FROM Televisions;
 
 -- add new engagement
 INSERT INTO `Enagements` (`favorite`, `rating`, `view`, `user_id`, `movie_id`, `television_id`)
 VALUES (:engagements_favorite_checkboxInput, :engagements_rating_from_dropdown_Input, :engagements_view_checkboxInput, 
 :engagements_user_idInput, :engagements_movie_title_from_dropdownInput, :engagements_television_title_from_dropdownInput);
 
--- Query for delete a new character functionality with colon : character being used to 
+-- add new movie
+INSERT INTO `Movies` (`movie_title`, `movie_length`)
+VALUES (:movies_titleInput, :movies_lengthInput);
+
+-- add movie's associated M:N relationships to genres, actors, directors
+ INSERT INTO Movies_Genres (movie_id_mg, genre_id_mg) VALUES (:movie_id_from_insert, :genre_id_from_checkboxInput)
+ INSERT INTO Movies_Actors (movie_id_ma, actor_id_ma) VALUES (:movie_id_from_dropdown_Input, :actor_id_from_checkboxInput)
+ INSERT INTO Movies_Directors (movie_id_md, director_id_md) VALUES (:movie_id_from_dropdown_Input, :director_id_from_checkboxInput);
+
+-- Queries for delete functionality with colon : character being used to 
 -- denote the variables that will have data from the backend programming language
 
 -- delete genre
@@ -68,4 +121,33 @@ DELETE FROM `Directors` WHERE director_id = :director_ID_selected_from_browse_di
 DELETE FROM `Episodes` WHERE episode_id = :episode_ID_selected_from_browse_episodes_page
 
 -- delete engagement
-DELETE FROM `Engagement` WHERE engagement_id = :engagement_ID_selected_from_browse_engagements_page
+DELETE FROM `Engagements` WHERE engagement_id = :engagement_ID_selected_from_browse_engagements_page
+
+-- delete movies
+DELETE FROM `Movies` WHERE movie_id = :movie_ID_selected_from_browse_movies_page
+
+-- delete televisions
+DELETE FROM `Televisions` WHERE television_id = :television_ID_selected_from_browse_televisions_page
+
+-- Queries for update functionality with colon : character being used to 
+-- denote the variables that will have data from the backend programming language
+
+-- update an engagement based on submission of the Update engagement
+SELECT engagement_id, favorite, rating, view, user_id, Movies.movie_title AS movie_title, Televisions.television_title AS television_title
+	FROM Engagements
+		LEFT JOIN Movies
+        ON Engagements.movie_id = Movies.movie_id 
+		LEFT JOIN Televisions
+        ON Engagements.television_id = Televisions.television_id
+	WHERE engagement_id = :engagement_ID_selected_from_browse_engagements_page;
+    
+UPDATE Engagements 
+	SET favorite = :engagements_favorite_checkboxInput, 
+		rating = :engagements_rating_from_dropdown_Input,
+        view = :engagements_view_checkboxInput, 
+        user_id = :engagements_user_idInput, 
+        movie_title = :engagements_movie_title_from_dropdownInput,
+        television_title = :engagements_television_title_from_dropdownInput
+	WHERE engagement_id = :engagement_id_from_update_submission
+        
+
